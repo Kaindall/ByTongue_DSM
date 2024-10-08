59f9c016cae1bcc49b3d92133c41fbe807aecc5f
Davi Ribeiro <daviribeiro442@gmail.com>
2024-10-07 23:15:51 -0300

    FEAT: mockar dados para teste

---
+++ src/main/application/web/view/quiz/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+   {
+      number: 1,
+      question: 'What is the correct form of the verb in the sentence: "She ___ to the park every morning"?',
+      answer:'goes',
+      alternatives: [
+         "go",
+         "goes",
+         "going",
+         "gone"
+      ]
+   },
+   {
+      number: 2,
+      question: 'How do you say "gato" in English?',
+      answer:'cat',
+      alternatives: [
+         "dog",
+         "bird",
+         "cat",
+         "fish"
+      ]
+   },
+   {
+      number: 3,
+      question: 'Which word is a color?',
+      answer:'red',
+      alternatives: [
+         "book",
+         "chair",
+         "red",
+         "table"
+      ]
+   },
+   {
+      number: 4,
+      question: 'What is the plural form of "child"?',
+      answer:'children',
+      alternatives: [
+         "childs",
+         "children",
+         "childes",
+         "childs"
+      ]
+   },
+   {
+      number: 5,
+      question: 'Which of the following is a greeting in English?',
+      answer:'Hello',
+      alternatives: [
+         "Goodbye",
+         "Thanks",
+         "Please",
+         "Hello"
+      ]
+   },
+   {
+      number: 6,
+      question: 'What is the opposite of "big"?',
+      answer:'small',
+      alternatives: [
+         "small",
+         "tall",
+         "short",
+         "large"
+      ]
+   },
+   {
+      number: 7,
+      question: 'How do you say "amarelo" in English?',
+      answer:'yellow',
+      alternatives: [
+         "yellow",
+         "blue",
+         "green",
+         "red"
+      ]
+   },
+   {
+      number: 8,
+      question: 'What is the correct article for the word "apple"?',
+      answer:'an',
+      alternatives: [
+         "a",
+         "an",
+         "the",
+         "one"
+      ]
+   },
+   {
+      number: 9,
+      question: 'What is the past tense of the verb "go"?',
+      answer:'went',
+      alternatives: [
+         "gone",
+         "goes",
+         "going",
+         "went"
+      ]
+   },
+   {
+      number: 10,
+      question: 'Which of the following words is a noun?',
+      answer:'car',
+      alternatives: [
+         "run",
+         "quickly",
+         "happy",
+         "car"
+      ]
+   }
+]
