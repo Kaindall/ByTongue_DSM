b4dc31e731d731dac249b22891593b8479822737
Mayara Buriti <mayara.clement@hotmail.com.br>
2024-12-02 08:03:03 -0300

    FEAT: validação de usuário logado

---
+++ src/main/application/web/view/login/validated-user.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const currentPath = window.location.pathname;
+  const loginPagePath = "/login";
+  const quizPagePath = "/quiz";
+
+  const userToken = sessionStorage.getItem("authToken");
+
+  try {
+      if (!userToken) {
+          if (currentPath !== loginPagePath) {
+              window.location.href = loginPagePath;
+          }
+          return;
+      }
+
+
+      const response = await fetch("http://localhost:8000/auth", {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${userToken}`
+          }
+      });
+
+      if (!response.ok) {
+          if (currentPath !== loginPagePath) {
+              window.location.href = loginPagePath;
+          }
+          return;
+      }
+
+      const data = await response.json();
+
+
+      if (!data.user_id) {
+          if (currentPath !== loginPagePath) {
+              window.location.href = loginPagePath;
+          }
+          return;
+      }
+
+      if (currentPath === loginPagePath) {
+          window.location.href = quizPagePath;
+      }
+  } catch (error) {
+      console.error("Erro ao verificar a sessão:", error);
+
+      if (currentPath !== loginPagePath) {
+          window.location.href = loginPagePath;
+      }
+  }
+});
