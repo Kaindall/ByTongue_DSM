1a701c7b33d2685beae1e7532d6e60de1aeb3a57
Kaindall <gustavozanete2@gmail.com>
2025-05-05 04:34:40 -0300

    FIX: colocar validação do arquivo .env

--- src/AppConfig.php
+++ src/AppConfig.php
@@ -61,17 +61,20 @@ abstract class AppConfig {
     private static function loadEnv() {
         $file = '.env';
 
-        $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        foreach ($lines as $line) {
-            if (strpos(trim($line), '#') === 0 || trim($line) === '') {
-                continue; // Ignora comentários e linhas vazias
+        if (file_exists($file)) {
+            $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+            foreach ($lines as $line) {
+                if (strpos(trim($line), '#') === 0 || trim($line) === '') {
+                    continue;
+                }
+                list($name, $value) = explode('=', $line, 2);
+                $name = trim($name);
+                $value = trim($value, '"');
+                
+                // Define apenas se a variável ainda não foi definida no ambiente
+                if (getenv($name) === false) {
+                    putenv("$name=$value");
+                }
             }
-
-            list($name, $value) = explode('=', $line, 2);
-            $name = trim($name);
-            $value = trim($value, '"'); // Remove aspas duplas
-
-            putenv("$name=$value");
         }
-    }
 }
