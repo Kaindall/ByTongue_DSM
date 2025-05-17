13f999a94fee4871f8cca01f4e20b76ccff9f41e
Kaindall <gustavozanete2@gmail.com>
2025-05-17 19:30:46 -0300

    FEAT: arrumar versao de IA do quiz

--- src/main/domain/service/ia/impl/GeminiService.php
+++ src/main/domain/service/ia/impl/GeminiService.php
@@ -16,7 +16,7 @@ class GeminiService implements IaService {
     }
 
     public function retrieveQuiz(array $params) {
-        $client = new GeminiClient("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$this->key");
+        $client = new GeminiClient("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=$this->key");
         if (!($client instanceof Quizzeable)) {throw new ObjectNotQuizzeableException;}
 
         $quizValidator = new QuizValidator();
